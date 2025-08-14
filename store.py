0308ce994be7393aa3c6baf96eee0a50b6cea3fa
tamim09-ctrl <thameemkareem050@gmail.com>
2025-08-14 13:30:20 +0530

    Add files via upload

---
+++ store.py
@@ -0,0 +1,6 @@
+products=(input("enter the what u want:"))
+quantity=int(input("how much u want:"))
+price=int(input("price of ur product as labelled:"))
+total=quantity * price
+print(products)
+print(total)
